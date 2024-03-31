735723cc6cd87c377d094dac26b71678516830e4
Your Name <nicode611@gmail.com>
2024-03-31 23:35:57 +0200

    23::55

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Hello World !</h1>
+    <div class="container">
+        <video width="320" height="240" controls autoplay>
+            <source src="chemin_de_la_video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
 </body>
 </html>
